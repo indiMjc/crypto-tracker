a38814e573204d6b75e6bd1c66dfb2f227ae54bb
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-22 13:50:02 -0500

    exchange cards are responsive

--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -16,7 +16,7 @@ const Coin = props => {
   };
 
   return (
-    <section className={`coin-card ${visClass}`} onClick={() => showThisCoin()}>
+    <section className="coin-card" id={visClass} onClick={() => showThisCoin()}>
       <div className="coin-img" style={filtered}>
         <p>{props.coin.symbol}</p>
       </div>
@@ -52,14 +52,17 @@ const Coin = props => {
                 <td>${props.coin.current_price}</td>
                 <td>${props.coin.market_cap}</td>
                 <td>{props.coin.market_cap_rank}</td>
-                <td>{props.coin.total_volume}</td>
-                <td>{props.coin.high_24h}</td>
-                <td>{props.coin.low_24h}</td>
+                <td>${props.coin.total_volume}</td>
+                <td>${props.coin.high_24h}</td>
+                <td>${props.coin.low_24h}</td>
                 <td>{props.coin.price_change_24h}</td>
-                <td>{props.coin.price_change_percentage_24h}</td>
+                <td>{props.coin.price_change_percentage_24h}%</td>
                 <td>{props.coin.market_cap_change_24h}</td>
-                <td>{props.coin.market_cap_change_percentage_24h}</td>
-                <td>{props.coin.circulating_supply}</td>
+                <td>{props.coin.market_cap_change_percentage_24h}%</td>
+                <td>
+                  {props.coin.circulating_supply}
+                  {props.coin.symbol}
+                </td>
                 <td>{props.coin.total_supply}</td>
                 <td>{props.coin.ath}</td>
                 <td>{props.coin.ath_change_percentage}</td>
@@ -68,68 +71,6 @@ const Coin = props => {
               </tr>
             </tbody>
           </table>
-
-          // <table>
-          //   <tr>
-          //     <td>{props.coin.name}</td>
-          //     <td>Symbol: {props.coin.symbol}</td>
-          //     <td>Current price: ${props.coin.current_price}</td>
-          //     <td>Market cap: ${props.coin.market_cap}</td>
-          //     <td>Market cap rank: {props.coin.market_cap_rank}</td>
-          //     <td>24hr volume: {props.coin.total_volume}</td>
-          //     <td>24hr high: {props.coin.high_24}</td>
-          //     <td>24hr low: {props.coin.low_24}</td>
-          //     <td>24hr price change: {props.coin.price_change_24}</td>
-          //     <td>
-          //       Price change percentage:{" "}
-          //       {props.coin.price_change_percentage_24h}
-          //     </td>
-          //   </tr>
-          //   <tr>
-          //     <td>Market cap change: {props.coin.market_cap_change_24h}</td>
-          //     <td>
-          //       Market cap change percentage:{" "}
-          //       {props.coin.market_cap_change_percentage_24h}
-          //     </td>
-          //     <td>Circulating supply: {props.coin.circulating_supply}</td>
-          //     <td>Total supply: {props.coin.total_supply}</td>
-          //     <td>All time high: {props.coin.ath}</td>
-          //     <td>
-          //       All time high change percentage:{" "}
-          //       {props.coin.ath_change_percentage}
-          //     </td>
-          //     <td>All time high date: {props.coin.ath_date}</td>
-          //     <td>Last updated: {props.coin.last_updated}</td>
-          //   </tr>
-          // </table>
-          //   <>
-          //     <h1>{props.coin.name}</h1>
-          //     <p>Symbol: {props.coin.symbol}</p>
-          //     <p>Current price: ${props.coin.current_price}</p>
-          //     <p>Market cap: ${props.coin.market_cap}</p>
-          //     <p>Market cap rank: {props.coin.market_cap_rank}</p>
-          //     <p>24hr volume: {props.coin.total_volume}</p>
-          //     <p>24hr high: {props.coin.high_24}</p>
-          //     <p>24hr low: {props.coin.low_24}</p>
-          //     <p>24hr price change: {props.coin.price_change_24}</p>
-          //     <p>
-          //       Price change percentage: {props.coin.price_change_percentage_24h}
-          //     </p>
-          //     <p>Market cap change: {props.coin.market_cap_change_24h}</p>
-          //     <p>
-          //       Market cap change percentage:{" "}
-          //       {props.coin.market_cap_change_percentage_24h}
-          //     </p>
-          //     <p>Circulating supply: {props.coin.circulating_supply}</p>
-          //     <p>Total supply: {props.coin.total_supply}</p>
-          //     <p>All time high: {props.coin.total_supply}</p>
-          //     <p>
-          //       All time high change percentage:{" "}
-          //       {props.coin.ath_change_percentage}
-          //     </p>
-          //     <p>All time high date: {props.coin.ath_date}</p>
-          //     <p>Last updated: {props.coin.last_updated}</p>
-          //   </>
         )}
       </section>
     </section>
