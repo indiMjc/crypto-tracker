a22d49fb34f9b0eab96fc38993c9792cb6748c5c
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-23 22:51:48 -0500

    deleted commented out styles and tweaked size of top50 page to be exactly 100vh

--- src/components/coins/CoinModal.js
+++ src/components/coins/CoinModal.js
@@ -6,6 +6,7 @@ const CoinModal = props => {
   const filtered = {
     backgroundImage: `url(${props.coin.image})`
   };
+
   return (
     <section className="coin-modal" id={props.visClass}>
       <section className="modal-header">
@@ -98,55 +99,6 @@ const CoinModal = props => {
               </table>
             </div>
           </div>
-          {/* <table>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Symbol</th>
-                <th>Spot price</th>
-                <th>Market cap</th>
-                <th>Market cap rank</th>
-                <th>24hr volume</th>
-                <th>24hr high</th>
-                <th>24hr low</th>
-                <th>24hr change, total</th>
-                <th>24hr change, percentage</th>
-                <th>24hr market cap change, total</th>
-                <th>24hr market cap change, percentage</th>
-                <th>Circulating supply</th>
-                <th>Total supply</th>
-                <th>All time high</th>
-                <th>Change from all time high</th>
-                <th>All time high date</th>
-                <th>Last updated</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{props.coin.name}</td>
-                <td>{props.coin.symbol}</td>
-                <td>${props.coin.current_price}</td>
-                <td>${props.coin.market_cap}</td>
-                <td>{props.coin.market_cap_rank}</td>
-                <td>${props.coin.total_volume}</td>
-                <td>${props.coin.high_24h}</td>
-                <td>${props.coin.low_24h}</td>
-                <td>{props.coin.price_change_24h}</td>
-                <td>{props.coin.price_change_percentage_24h}%</td>
-                <td>{props.coin.market_cap_change_24h}</td>
-                <td>{props.coin.market_cap_change_percentage_24h}%</td>
-                <td>
-                  {props.coin.circulating_supply}
-                  {props.coin.symbol}
-                </td>
-                <td>{props.coin.total_supply}</td>
-                <td>{props.coin.ath}</td>
-                <td>{props.coin.ath_change_percentage}</td>
-                <td>{props.coin.ath_date}</td>
-                <td>{props.coin.last_updated}</td>
-              </tr>
-            </tbody>
-          </table> */}
         </section>
         <FontAwesomeIcon
           icon={faWindowClose}
