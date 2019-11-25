e8d1396dcea6fe514930f64cc86b402487ecf6d5
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-24 20:45:54 -0500

    emotion added, stared abstracting css away from Coin.js

--- src/components/coins/Coin.js
+++ src/components/coins/Coin.js
@@ -1,6 +1,29 @@
-import React, { useState } from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import { useState } from "react";
 import CoinModal from "./CoinModal";
 import "./Coin.css";
+
+const cardStyle = css`
+  display: flex;
+  height: 15vh;
+  width: 15vh;
+  border-width: 5px;
+  border-style: solid;
+  border-image: linear-gradient(-45deg, rgb(255, 255, 255), rgb(80, 80, 80));
+  border-radius: 0.3rem;
+  padding: 1vh;
+  margin: 2vh 2vh 0;
+  background: linear-gradient(45deg, rgb(255, 255, 255), rgb(80, 80, 80));
+  box-shadow: 1rem 1rem 0.5rem rgba(0, 0, 0, 0.164);
+  font-size: 2.6rem;
+  transition: all 0.2s ease;
+
+  &:hover {
+    box-shadow: 0 0 0.2rem white, 0 0 0.5rem white, 0 0 0.8rem white,
+      0 0 1.5rem white, 0 0 1.8rem white, 0 0 2.1rem white;
+  }
+`;
 
 const Coin = props => {
   const [vis, setVis] = useState(false);
@@ -17,12 +40,13 @@ const Coin = props => {
   };
 
   return (
-    <section className="coin-card">
+    <section css={cardStyle}>
       <div className="coin-img" style={filtered} onClick={() => showThisCoin()}>
         <p>{props.coin.symbol}</p>
       </div>
       {vis && (
         <CoinModal
+          styles={cardStyle}
           coin={props.coin}
           visClass={visClass}
           showThisCoin={showThisCoin}
