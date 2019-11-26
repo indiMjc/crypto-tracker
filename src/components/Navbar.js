dce22b00117b31ccc58383be89630836da8ce02a
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-11-26 10:10:40 -0500

    removed logger and added action that increases the coin page count by one in store

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCoins } from "../actions";
 
 const Navbar = () => {
   const dispatch = useDispatch();
+  const pageNum = useSelector(state => state.coinPageNum);
 
   return (
     <>
       <Link to="/exchangeData">View Exchange Data</Link>
-      <Link onClick={() => dispatch(getCoins())} to="/top50coins">
+      <Link onClick={() => dispatch(getCoins(pageNum))} to="/top50coins">
         View Coin Data
       </Link>
     </>
--- src/index.js
+++ src/index.js
@@ -5,17 +5,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { applyMiddleware, createStore } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../src/reducers/";
 
 import "./index.css";
 import App from "./App";
 
-const store = createStore(
-  reducer,
-  composeWithDevTools(applyMiddleware(thunk, logger))
-);
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
